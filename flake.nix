8cd51af1e487ac56b12bdb465a4afa35d46505f0
Emory Merryman <emory.merryman@gmail.com>
2025-07-15 22:09:34 -0400

    fixed typo

--- flake.nix
+++ flake.nix
@@ -448,10 +448,10 @@ EOF
 														        in
                                                                     ''
                                                                         mkdir --parents $out/bin
+                                                                            # "--run "\export NAME=\$( ${ repository } )\"" \
                                                                         makeWrapper \
                                                                             ${ user-env }/bin/${ name } \
                                                                             $out/bin/${ name } \
-                                                                            # "--run "\export NAME=\$( ${ repository } )\"" \
                                                                             "--run \"export HOME=/${ name }/home\"" \
                                                                             "--run \"export GIT_DIR=/${ name }/git\"" \
                                                                             "--run \"export GIT_WORK_TREE=/${ name }/work-tree\""
