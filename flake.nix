7b841bea611782a41169ab25e52386f902f2cf2c
Emory Merryman <emory.merryman@gmail.com>
2025-07-24 19:47:10 -0400

    WTF

--- flake.nix
+++ flake.nix
@@ -155,7 +155,7 @@
                                                                 source =
                                                                     let
                                                                         repository =
-                                                                            origin : input-script : sed : ignore :
+                                                                            name : origin : input-script : sed : ignore :
                                                                                 {
                                                                                     init-inputs = [ pkgs.coreutils pkgs.git ] ;
                                                                                     init-text =
@@ -216,14 +216,14 @@
                                                                                 } ;
                                                                         in
                                                                             {
-                                                                                private = repository config.personal.repository.private.remote resources.repository.private true ;
+                                                                                private = repository "private" config.personal.repository.private.remote resources.repository.private true ;
                                                                                 inputs =
                                                                                     {
-                                                                                        # applications = repository config.personal.repository.applications.remote resources.repository.applications false ;
-                                                                                        personal = repository config.personal.repository.personal.remote resources.repository.personal false ;
-                                                                                        secret = repository config.personal.repository.secret.remote resources.repository.secret false ;
-                                                                                        secrets = repository config.personal.repository.secrets.remote resources.repository.secrets false ;
-                                                                                        visitor = repository config.personal.repository.visitor.remote resources.repository.visitor false ;
+                                                                                        # applications = repository "applications" config.personal.repository.applications.remote resources.repository.applications false ;
+                                                                                        personal = repository "personal" config.personal.repository.personal.remote resources.repository.personal false ;
+                                                                                        secret = repository "secret" config.personal.repository.secret.remote resources.repository.secret false ;
+                                                                                        secrets = repository "secrets" config.personal.repository.secrets.remote resources.repository.secrets false ;
+                                                                                        visitor = repository "visitor" config.personal.repository.visitor.remote resources.repository.visitor false ;
                                                                                     } ;
                                                                             } ;
                                                                 virtual-machine =
