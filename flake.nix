31beef703bbd70de0d4809fcf8faa9b785556e1a
Emory Merryman <emory.merryman@gmail.com>
2025-07-24 17:43:57 -0400

    it actually workd

--- flake.nix
+++ flake.nix
@@ -190,7 +190,6 @@
                                                                                                 esac
                                                                                             done
                                                                                             echo "SHIFT=$SHIFT"
-                                                                                            shift
                                                                                             export GIT_DIR="$SELF/git"
                                                                                             export GIT_WORK_TREE="$SELF/work-tree"
                                                                                             mkdir --parents "$GIT_DIR"
@@ -210,13 +209,13 @@
                                                                                             COMMIT="$( GIT_DIR="$INPUT/git" GIT_WORK_TREE="$INPUT/work-tree" git rev-parse HEAD )"
                                                                                             git checkout "$COMMIT"
                                                                                             ${ if sed then "git fetch origin scratch/f91bb4c0-5c10-41f0-bb3c-cab9bd3ee3fc && git checkout scratch/f91bb4c0-5c10-41f0-bb3c-cab9bd3ee3fc" else "# " }
-                                                                                            # ${ if sed then "# shellcheck disable=SC2027,SC2086,SC2068" else "#" }
-                                                                                            # ${ if sed then ''sed -i ${ builtins.concatStringsSep " " ( builtins.attrValues ( builtins.mapAttrs ( name : value : ''-e "s#\(${ name }.url.*?ref=\)main\(\".*\)\$#\1\$( GIT_DIR=\$( ${ value } )/git GIT_WORK_TREE=\$( ${ value } )/work-tree ${ pkgs.git }/bin/git rev-parse HEAD )\2#"'' ) resources.milestone.source.inputs ) ) } "$GIT_WORK_TREE/flake.nix"'' else "# " }
-                                                                                            # for OVERRIDE_INPUT in "${ builtins.concatStringsSep "" [ "$" "{" "OVERRIDE_INPUTS[@]" "}" ] }"
-                                                                                            # do
-                                                                                            #     sed -i -e "$OVERRIDE_INPUT" "$GIT_WORK_TREE/flake.nix"
-                                                                                            #     shift 2
-                                                                                            # done
+                                                                                            ${ if sed then "# shellcheck disable=SC2027,SC2086,SC2068" else "#" }
+                                                                                            ${ if sed then ''sed -i ${ builtins.concatStringsSep " " ( builtins.attrValues ( builtins.mapAttrs ( name : value : ''-e "s#\(${ name }.url.*?ref=\)main\(\".*\)\$#\1\$( GIT_DIR=\$( ${ value } )/git GIT_WORK_TREE=\$( ${ value } )/work-tree ${ pkgs.git }/bin/git rev-parse HEAD )\2#"'' ) resources.milestone.source.inputs ) ) } "$GIT_WORK_TREE/flake.nix"'' else "# " }
+                                                                                            for OVERRIDE_INPUT in "${ builtins.concatStringsSep "" [ "$" "{" "OVERRIDE_INPUTS[@]" "}" ] }"
+                                                                                            do
+                                                                                                sed -i -e "$OVERRIDE_INPUT" "$GIT_WORK_TREE/flake.nix"
+                                                                                                shift 2
+                                                                                            done
                                                                                         '' ;
                                                                                 } ;
                                                                         in
