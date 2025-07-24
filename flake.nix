14857b9cb36aec5e5fd6685aec4f713a76ec66c6
Emory Merryman <emory.merryman@gmail.com>
2025-07-24 09:39:10 -0400

    yes

--- flake.nix
+++ flake.nix
@@ -130,7 +130,7 @@
                                                                                             COMMIT="$( GIT_DIR="$INPUT/git" GIT_WORK_TREE="$INPUT/work-tree" git rev-parse HEAD )"
                                                                                             git checkout "$COMMIT"
                                                                                             ${ if sed then "git fetch origin scratch/f91bb4c0-5c10-41f0-bb3c-cab9bd3ee3fc && git checkout scratch/f91bb4c0-5c10-41f0-bb3c-cab9bd3ee3fc" else "# " }
-                                                                                            sed -i ${ builtins.concatStringsSep " " ( builtins.attrValues ( builtins.mapAttrs ( name : value : ''-e "s#?ref=main#?ref=REVISION_TOKEN#"'' ) resources.milestone.source.inputs ) ) } "$GIT_WORK_TREE/flake.nix"
+                                                                                            sed -i ${ builtins.concatStringsSep " " ( builtins.attrValues ( builtins.mapAttrs ( name : value : ''-e "s#?ref=main#?ref=$( GIT_DIR=${ value }/git GIT_WORK_TREE=${ value }/work-tree git HEAD )#"'' ) resources.milestone.source.inputs ) ) } "$GIT_WORK_TREE/flake.nix"
                                                                                         '' ;
                                                                                 } ;
                                                                         in
