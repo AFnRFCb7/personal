190ce9915b1adefdee81889959e327d7eefd41ed
Emory Merryman <emory.merryman@gmail.com>
2025-07-24 13:00:40 -0400

    WTF

--- flake.nix
+++ flake.nix
@@ -170,7 +170,7 @@
                                                                                             git config user.email ${ config.personal.email }
                                                                                             git config user.name "${ config.personal.name }"
                                                                                             git remote add origin "${ origin }"
-                                                                                            INPUT="$( "${ input-script }" "$1" )"
+                                                                                            INPUT="$( "${ input-script }" FIXME )"
                                                                                             echo "$INPUT"
                                                                                             GIT_DIR="$INPUT/git" GIT_WORK_TREE="$INPUT/work-tree" git add .
                                                                                             GIT_DIR="$INPUT/git" GIT_WORK_TREE="$INPUT/work-tree" git commit -am "" --allow-empty --allow-empty-message
