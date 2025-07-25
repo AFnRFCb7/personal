24e04e8777d4f67f7d31b018224e289d78dce337
Emory Merryman <emory.merryman@gmail.com>
2025-07-24 20:28:44 -0400

    WTF

--- flake.nix
+++ flake.nix
@@ -526,10 +526,10 @@
                                                                                                                     ''
                                                                                                                         SOURCE="$( ${ resources.milestone.source.private } "$@" )"
                                                                                                                         head "$SOURCE/work-tree/flake.nix"
-                                                                                                                        export NIX_LOG=trace
-                                                                                                                        export NIX_SHOW_TRACE=1
-                                                                                                                        cd "$SOURCE/work-tree"
-                                                                                                                        nix flake check --print-build-logs --verbose --verbose --verbose
+                                                                                                                        # export NIX_LOG=trace
+                                                                                                                        # export NIX_SHOW_TRACE=1
+                                                                                                                        # cd "$SOURCE/work-tree"
+                                                                                                                        # nix flake check --print-build-logs --verbose --verbose --verbose
                                                                                                                         # CHECK="$( ${ resources.milestone.check } "$@" )"
                                                                                                                         # echo "CHECK=$CHECK"
                                                                                                                         # BUILD_VM="$( ${ resources.milestone.virtual-machine.build } "$@" )"
