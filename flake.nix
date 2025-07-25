ca80bb5d7156a500dfd94459cfa76fce6bdac7e7
Emory Merryman <emory.merryman@gmail.com>
2025-07-25 18:19:19 -0400

    why

--- flake.nix
+++ flake.nix
@@ -151,7 +151,7 @@
                                                                                     export NIX_SHOW_TRACE=1
                                                                                     echo c1bf2545-13b2-4a2f-bf19-ae7a8b66822f > /tmp/DEBUG
                                                                                     cat $( ${ resources.milestone.source.private } "$@" )/work-tree/flake.nix >> /tmp/DEBUG
-                                                                                    echo time timeout ${ builtins.toString config.personal.milestone.timeout } nix flake check --print-build-logs --verbose --verbose --verbose "$( ${ resources.milestone.source.private } "$@" )/work-tree" >> /tmp/DEBUG 2>&1
+                                                                                    echo "time timeout ${ builtins.toString config.personal.milestone.timeout } nix flake check --print-build-logs --verbose --verbose --verbose \"\$( ${ resources.milestone.source.private } "$@" )/work-tree\"" >> /tmp/DEBUG 2>&1
                                                                                     time timeout ${ builtins.toString config.personal.milestone.timeout } nix flake check --print-build-logs --verbose --verbose --verbose "$( ${ resources.milestone.source.private } "$@" )/work-tree"
                                                                                     echo af4690ae-c213-46fa-a9d6-31d538d036b0 >> /tmp/DEBUG
                                                                                 '' ;
