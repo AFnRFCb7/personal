545197723fd6a63f9cd3a308bada1545bcd6a0bc
Emory Merryman <emory.merryman@gmail.com>
2025-07-25 11:00:29 -0400

    test

--- flake.nix
+++ flake.nix
@@ -178,6 +178,10 @@
                                                                                     echo "shift: $SHIFT" >> "$SELF/configuration.yaml"
                                                                                 '' ;
                                                                         } ;
+                                                                promote =
+                                                                    ignore :
+                                                                        {
+                                                                        } ;
                                                                 source =
                                                                     let
                                                                         repository =
@@ -253,11 +257,8 @@
                                                                                             echo c4a13718-4023-4cf1-b048-a40f47c63aca >> /tmp/DEBUG
                                                                                             mkdir --parents "$SHARED_DIR"
                                                                                             echo 176849c1-fa91-4af1-b10e-4fe71f4d67e4 >> /tmp/DEBUG
-                                                                                            cat >> /tmp/DEBUG <<EOF
-                                                                                            VM="$( ${ resources.milestone.virtual-machine.build } )"
+                                                                                            VM=$( ${ resources.milestone.virtual-machine.build } )
                                                                                             "$VM/result/bin/run-nixos-vm" -nographic
-                                                                                            EOF
-                                                                                            ${ resources.milestone.virtual-machine.build }/result/bin/run-nixos-vm -nographic
                                                                                             echo d5f6a6f4-96cb-43f4-b965-262ccb5a7a45 >> /tmp/DEBUG
                                                                                         '' ;
                                                                                 } ;
