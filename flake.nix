71603c27fc157306e8d5cc35101f761b84eabf90
emory.merryman@gmail.com <Emory Merryman>
2025-07-27 10:55:13 -0400

    WTF

--- flake.nix
+++ flake.nix
@@ -90,7 +90,7 @@
                                                                     EOF
                                                                     git init
                                                                     ${ builtins.concatStringsSep "\n" ( builtins.attrValues ( builtins.mapAttrs ( name : value : ''git config "${ name }" "${ value }"'' ) configs ) ) }
-                                                                    ${ builtins.concatStringsSep "\n" ( builtins.attrValues ( builtins.mapAttrs ( name : value : ''ln --symbolic "${ value }" "$GIT_DIR/hooks${ name }"'' ) hooks ) ) }
+                                                                    ${ builtins.concatStringsSep "\n" ( builtins.attrValues ( builtins.mapAttrs ( name : value : ''ln --symbolic "${ value }" "$GIT_DIR/hooks/${ name }"'' ) hooks ) ) }
                                                                     ${ builtins.concatStringsSep "\n" ( builtins.attrValues ( builtins.mapAttrs ( name : value : ''remote add "${ name }" "${ value }"'' ) remotes ) ) }
                                                                     ${ if builtins.typeOf setup == "string" then setup else "" }
                                                                 '' ;
