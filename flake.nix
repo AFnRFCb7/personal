fd0c67a9f2817c34e417e515f1d7bb2142b7b4f7
emory.merryman@gmail.com <Emory Merryman>
2025-07-27 11:53:22 -0400

    WTF

--- flake.nix
+++ flake.nix
@@ -296,7 +296,6 @@
                                                                                                                         REPO="$1"
                                                                                                                         GIT_DIR="$REPO/git" GIT_WORK_TREE="$REPO/work-tree" git rev-parse HEAD
                                                                                                                     }
-                                                                                                                SNAPSHOT="$( ${ resources.milestone.snapshot } --link root "$SELF" "$( commit "$SELF" )" --link input "$SELF/inputs/personal" "$( commit "$SELF/inputs/personal" )" --link "$SELF/inputs/secret" "$( commit "$SELF/inputs/secret" )" )"
                                                                                                                 echo "$SNAPSHOT"
                                                                                                             '' ;
                                                                                                     } ;
