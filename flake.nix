505c3fc3c4ba597522e2d1146939531433117a64
emory.merryman@gmail.com <Emory Merryman>
2025-07-28 12:44:36 -0400

    WTF

--- flake.nix
+++ flake.nix
@@ -294,7 +294,7 @@
                                                                                                         git checkout "$ZCOMMIT"
                                                                                                         find $LOCO/root/local/inputs -mindepth 1 -maxdepth 1 -type l | while read LINK
                                                                                                         do
-                                                                                                            sed -i "s#\($( ).url.*?ref=\)main#\1$ZCOMMIT#" "$GIT_WORK_TREE/flake.nix"
+                                                                                                            sed -i "s#\($( basename "$LINK" ).url.*?ref=\)main#\1$ZCOMMIT#" "$GIT_WORK_TREE/flake.nix"
                                                                                                         done
                                                                                                     '' ;
                                                                                             } ;
