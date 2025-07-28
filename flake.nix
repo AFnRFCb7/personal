f3d238bf67e951244a169bf5cedf0314c4208f0c
emory.merryman@gmail.com <Emory Merryman>
2025-07-27 22:02:26 -0400

    one small fix

--- flake.nix
+++ flake.nix
@@ -289,7 +289,7 @@
                                                                                                     ''
                                                                                                         git fetch remote
                                                                                                         echo git fetch remote "$ZBRANCH" > /tmp/DEBUG
-                                                                                                        git fetch remote "$ZBRANCH"
+                                                                                                        git fetch remote "$ZBRANCH" >> /tmp/DEBUG 2>&1
                                                                                                         # git fetch remote "$ZBRANCH"
                                                                                                         # git checkout "ZCOMMIT"
                                                                                                     '' ;
