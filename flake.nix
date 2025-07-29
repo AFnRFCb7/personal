6e22eec357d189762f6a0e93bec81fbd903c4692
emory.merryman@gmail.com <Emory Merryman>
2025-07-29 09:13:06 -0400

    WTF

--- flake.nix
+++ flake.nix
@@ -291,6 +291,7 @@
                                                                                                         echo "fa2bdc45-bb63-49fb-bd3b-b490b28743d6" > /tmp/DEBUG
                                                                                                         LOCO20="$( ${ resources.milestone.snapshot } "$@" )"
                                                                                                         REMOTE="$( < "$LOCO20/root/remote" )"
+                                                                                                        find "$LOCO20/root" -maxdepth 1 -type f >> /tmp/DEBUG
                                                                                                         COMMIT="$( < "$LOCO20/root/commit" )"
                                                                                                         git fetch remote "$REMOTE"
                                                                                                         echo "90170a8e-b7a3-44b9-b881-878b08926451" >> /tmp/DEBUG
