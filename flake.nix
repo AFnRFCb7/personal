80dfbdbb79c5ab504a23197ea89c5cd7e442215b
emory.merryman@gmail.com <Emory Merryman>
2025-07-29 08:31:29 -0400

    WTF

--- flake.nix
+++ flake.nix
@@ -97,7 +97,7 @@
                                                                     ${ builtins.concatStringsSep "\n" ( builtins.attrValues ( builtins.mapAttrs ( name : value : ''git config "${ name }" "${ value }"'' ) configs ) ) }
                                                                     ${ builtins.concatStringsSep "\n" ( builtins.attrValues ( builtins.mapAttrs ( name : value : ''ln --symbolic "${ value }" "$GIT_DIR/hooks/${ name }"'' ) hooks ) ) }
                                                                     ${ builtins.concatStringsSep "\n" ( builtins.attrValues ( builtins.mapAttrs ( name : value : ''git remote add "${ name }" "${ value }"'' ) remotes ) ) }
-                                                                    ${ if builtins.typeOf setup == "string" then setup else "" }
+                                                                    ${ if builtins.typeOf setup == "string" then exec setup else "" }
                                                                 '' ;
                                                             release-inputs = release-inputs ;
                                                             release-text = release-text ;
