5005b4a36effd45108d89a26266c956c388936bc
emory.merryman@gmail.com <Emory Merryman>
2025-07-29 09:20:47 -0400

    WTF

--- flake.nix
+++ flake.nix
@@ -292,6 +292,7 @@
                                                                                                         REMOTE="$( < "$LOCO20/root/remote" )"
                                                                                                         COMMIT="$( < "$LOCO20/root/commit" )"
                                                                                                         echo "c515cbcf-5f3c-4308-ba71-a5291c096f78 $REMOTE $COMMIT" > /tmp/DEBUG
+                                                                                                        git remote --verbose >> /tmp/DEBUG 2>&1
                                                                                                         git fetch remote "$REMOTE" >> /tmp/DEBUG 2>&1
                                                                                                         echo "90170a8e-b7a3-44b9-b881-878b08926451" >> /tmp/DEBUG
                                                                                                         git checkout "$COMMIT"
