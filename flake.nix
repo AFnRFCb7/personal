2f016f5836d9494147746b415e48f72840b102d4
Emory Merryman <emory.merryman@gmail.com>
2025-07-30 08:59:21 -0400

    add source root

--- flake.nix
+++ flake.nix
@@ -458,7 +458,7 @@
                                                                                                         mkdir --parents "$SELF/inputs"
                                                                                                         ln --symbolic "$( ${ resources.repository.personal } "$@" )" "$SELF/inputs/personal"
                                                                                                         ln --symbolic "$( ${ resources.repository.secret } "$@" )" "$SELF/inputs/secret"
-                                                                                                        ln --symbolic "$( ${ resources.repository.secrets } "$@" )" "$SELF/inputs/secrets"
+                                                                                                        # ln --symbolic "$( ${ resources.repository.secrets } "$@" )" "$SELF/inputs/secrets"
                                                                                                         ln --symbolic "$( ${ resources.repository.visitor } "$@" )" "$SELF/inputs/visitor"
                                                                                                     '' ;
                                                                                             } ;
