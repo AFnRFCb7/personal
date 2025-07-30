a402c4d891285289b6fdd82b5493382bf742cf62
Emory Merryman <emory.merryman@gmail.com>
2025-07-30 12:54:12 -0400

    add source root

--- flake.nix
+++ flake.nix
@@ -344,7 +344,7 @@
                                                                                                 export NIX_SHOW_STATS=5
                                                                                                 export NIX_DEBUG=1
                                                                                                 echo cf797c47-c739-414f-b46f-bc2752c2e32c > /tmp/DEBUG
-                                                                                                nixos-rebuild build-vm${ if bootloader then "-with-bootloader" else "" } --flake "$DIRECTORY/work-tree#tester" --verbose --show-trace >> /tmp/DEBUG 2>&1
+                                                                                                nixos-rebuild build-vm${ if bootloader then "-with-bootloader" else "" } --flake "$( ${ resources.milestone.source.root } "$@" )/work-tree#tester" --verbose --show-trace >> /tmp/DEBUG 2>&1
                                                                                                 echo 4881be74-e87b-4667-97b5-f8714e8f915e >> /tmp/DEBUG
                                                                                             fi
                                                                                         '' ;
