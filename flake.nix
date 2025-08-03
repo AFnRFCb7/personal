305c66610555904985367a8f01a190625fd3eac4
Emory Merryman <emory.merryman@gmail.com>
2025-08-03 10:27:58 -0400

    fixed

--- flake.nix
+++ flake.nix
@@ -582,7 +582,7 @@
                                                                                                                 (
                                                                                                                     SHARED_DIR="$ROOT/vm/run/test"
                                                                                                                     export SHARED_DIR
-                                                                                                                    mkdir --parents "SHARED_DIR"
+                                                                                                                    mkdir --parents "$SHARED_DIR"
                                                                                                                     cd "$ROOT/vm/run"
                                                                                                                     if timeout ${ builtins.toString config.personal.milestone.timeout2 } "$ROOT/vm/build/result/bin/run-nixos-vm" -nographic > "$ROOT/vm/run/standard-output" 2> "$ROOT/vm/run/standard-error"
                                                                                                                     then
