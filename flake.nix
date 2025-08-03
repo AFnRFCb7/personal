bf59e1acfcb6bf35fac5e346d08c8bcce72b2675
Emory Merryman <emory.merryman@gmail.com>
2025-08-03 12:19:18 -0400

    get rid of timeout2

--- flake.nix
+++ flake.nix
@@ -584,7 +584,7 @@
                                                                                                                     export SHARED_DIR
                                                                                                                     mkdir --parents "$SHARED_DIR"
                                                                                                                     cd "$ROOT/vm/run"
-                                                                                                                    if timeout ${ builtins.toString config.personal.milestone.timeout2 } "$ROOT/vm/build/result/bin/run-nixos-vm" -nographic > "$ROOT/vm/run/standard-output" 2> "$ROOT/vm/run/standard-error"
+                                                                                                                    if "$ROOT/vm/build/result/bin/run-nixos-vm" -nographic > "$ROOT/vm/run/standard-output" 2> "$ROOT/vm/run/standard-error"
                                                                                                                     then
                                                                                                                         echo "$?" > "$ROOT/vm/run/status"
                                                                                                                         echo "Ran vm $( date ) $( elapsed )"
@@ -623,7 +623,7 @@
                                                                                                                     export SHARED_DIR
                                                                                                                     mkdir --parents "$SHARED_DIR"
                                                                                                                     cd "$ROOT/vm-with-bootloader/run"
-                                                                                                                    if ${ builtins.toString config.personal.milestone.timeout2 } "$ROOT/vm-with-bootloader/build/result/bin/run-nixos-vm" -nographic > "$ROOT/vm-with-bootloader/run/standard-output" 2> "$ROOT/vm-with-bootloader/run/standard-error"
+                                                                                                                    if "$ROOT/vm-with-bootloader/build/result/bin/run-nixos-vm" -nographic > "$ROOT/vm-with-bootloader/run/standard-output" 2> "$ROOT/vm-with-bootloader/run/standard-error"
                                                                                                                     then
                                                                                                                         echo "$?" > "$ROOT/vm-with-bootloader/run/status"
                                                                                                                         echo "Ran vm-with-bootloader $( date ) $( elapsed )"
