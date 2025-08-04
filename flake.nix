de841550090a5f50b909931f0a4a3a345df8daf0
Emory Merryman <emory.merryman@gmail.com>
2025-08-04 13:21:00 -0400

    BITTER

--- flake.nix
+++ flake.nix
@@ -291,7 +291,7 @@
                                                                                                         git rebase "$SCRATCH"
                                                                                                     '' ;
                                                                                             } ;
-                                                                                    in "" # ; "${ setup }/bin/setup" ;
+                                                                                    in "" ; #  "${ setup }/bin/setup" ;
                                                                         } ;
                                                             } ;
                                                         repository =
