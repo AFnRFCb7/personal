2418ccb0441c8168d9bd594d43d93b75b7d2d4a2
Emory Merryman <emory.merryman@gmail.com>
2025-08-29 15:09:47 -0400

    YES

--- flake.nix
+++ flake.nix
@@ -1024,6 +1024,7 @@
                                                         ( resources-fun false false ( self + "/expected/false/false/0/checkpoint-pre" ) ( self + "/expected/false/false/0/checkpoint-post" ) [ ] )
                                                         ( resources-fun false true ( self + "/expected/false/true/0/checkpoint-pre" ) ( self + "/expected/false/true/0/checkpoint-post" ) [ { command = "/build/resources/recovery/0000000000000002/repair" ; checkpoint = self + "/expected/false/true/1/checkpoint" ; } ] )
                                                         ( resources-fun true false ( self + "/expected/true/false/0/checkpoint-pre" ) ( self + "/expected/true/false/0/checkpoint-post" ) [ { command = "/build/resources/recovery/0000000000000002/repair" ; checkpoint = self + "/expected/true/false/1/checkpoint" ; } ] )
+                                                        ( resources-fun true true ( self + "/expected/true/true/0/checkpoint-pre" ) ( self + "/expected/true/true/0/checkpoint-post" ) [ { command = "/build/resources/recovery/0000000000000002/repair" ; checkpoint = self + "/expected/true/true/1/checkpoint" ; } ] )
                                                         # ( resources-fun false true false )
                                                         # ( resources-fun false true true )
                                                         # ( resources-fun true false false )
