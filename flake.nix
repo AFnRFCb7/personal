d6c51b9d7643025eb0105c4a6f0c926b15f0a172
Emory Merryman <emory.merryman@gmail.com>
2025-10-08 11:52:35 -0400

    check 16

--- flake.nix
+++ flake.nix
@@ -998,7 +998,7 @@
                                                                                                                 BRANCH="$1"
                                                                                                                 COMMIT="$2"
                                                                                                                 cat >> /mount/.envrc <<EOF
-                                                                                                                export SOURCE=${ self }
+                                                                                                                export SOURCE="$REPOSITORY_ROOT"
                                                                                                                 export BRANCH="$BRANCH"
                                                                                                                 export COMMIT="$COMMIT"
                                                                                                                 EOF
