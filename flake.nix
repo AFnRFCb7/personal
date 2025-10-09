fbd99f1f7acee4d790d26f89398f4a1c8912e265
Emory Merryman <emory.merryman@gmail.com>
2025-10-09 10:59:17 -0400

    check 20

--- flake.nix
+++ flake.nix
@@ -737,9 +737,7 @@
                                                                                                                                 BRANCH="$( git rev-parse --abbrev-ref HEAD )" || exit 65
                                                                                                                                 COMMIT="$( git rev-parse HEAD )" || exit 66
                                                                                                                                 echo "${ resources_.promotion.root } \"$BRANCH\" \"$COMMIT\""
-                                                                                                                                PROMOTION="$( ${ resources_.promotion.root } "$BRANCH" "$COMMIT" )" || exit 67
-                                                                                                                                mkdir --parents "$REPOSITORY_ROOT/promotions"
-                                                                                                                                ln --symbolic "$PROMOTION" "$REPOSITORY_ROOT/promotions/$COMMIT"
+                                                                                                                                ${ resources_.promotion.root } "$BRANCH" "$COMMIT" )
                                                                                                                             '' ;
                                                                                                                     } ;
                                                                                                                 in "!${ application }/bin/promote" ;
