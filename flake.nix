12a3eac2365525573df451682dde177fb87eac3c
Emory Merryman <emory.merryman@gmail.com>
2025-10-09 10:13:10 -0400

    check 19

--- flake.nix
+++ flake.nix
@@ -831,6 +831,7 @@
                                                                                                                                         text =
                                                                                                                                             ''
                                                                                                                                                 nixos-rebuild test --flake "$SOURCE/work-tree#user"
+                                                                                                                                                exit 99
                                                                                                                                             '' ;
                                                                                                                                     } ;
                                                                                                                             in "${ application }/bin/test" ;
@@ -1080,6 +1081,8 @@
                                                                                                                                         TYPE="$2"
                                                                                                                                         DEPENDENT_BRANCH="$( GIT_DIR="$REPOSITORY_ROOT/git" GIT_WORK_TREE="$REPOSITORY_ROOT/work-tree" git config --get "dependents.$TYPE.branch" ) || exit 64
                                                                                                                                         DEPENDENT_COMMIT="$( GIT_DIR="$REPOSITORY_ROOT/git" GIT_WORK_TREE="$REPOSITORY_ROOT/work-tree" git config --get "dependents.$TYPE.commit" ) || exit 64
+                                                                                                                                        export DEPENDENT_BRANCH
+                                                                                                                                        export DEPENDENT_COMMIT
                                                                                                                                     '' ;
                                                                                                                             } ;
                                                                                                                     in "${ application }/bin/setup" ;
