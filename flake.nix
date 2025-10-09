8079550120e2f268b4ca006c1d0de7786a78b732
Emory Merryman <emory.merryman@gmail.com>
2025-10-09 14:13:44 -0400

    check 20

--- flake.nix
+++ flake.nix
@@ -1081,7 +1081,7 @@
                                                                                                                                         DEPENDENT_BRANCH="$( GIT_DIR="$SOURCE/git" GIT_WORK_TREE="$SOURCE/work-tree" git config --get "dependencies.$TYPE.branch" )" || exit 64
                                                                                                                                         git fetch origin "$DEPENDENT_BRANCH" 2>&1
                                                                                                                                         DEPENDENT_COMMIT="$( GIT_DIR="$SOURCE/git" GIT_WORK_TREE="$SOURCE/work-tree" git config --get "dependencies.$TYPE.commit" )" || exit 64
-                                                                                                                                        echo "$DEPENDENT_COMMIT" > "$GIT_DIR/wtf"
+                                                                                                                                        git checkout "$DEPENDENT_COMMIT" 2>&1
                                                                                                                                         # git checkout "$DEPENDENT_COMMIT"
                                                                                                                                         # git fetch origin main
                                                                                                                                         # if git diff --exit-code origin/main
