edafeedf181d503cffc22a23914a93fbe4ede619
Emory Merryman <emory.merryman@gmail.com>
2025-10-09 16:43:27 -0400

    check 20

--- flake.nix
+++ flake.nix
@@ -1081,11 +1081,11 @@
                                                                                                                                                     TOKEN="$( ${ resources_.secrets."github-token.asc.age" } )" || exit 64
                                                                                                                                                     echo "TOKEN=$TOKEN"
                                                                                                                                                     cat <<EOF
-                                                                                                                                                    gh auth login --host-name github.com --with-token < "$TOKEN/secret"
-                                                                                                                                                    URL="$( gh pr create --base main --head "$SQUASH_BRANCH" --title "Promotion" --body "Automated Promotion Merge" )" || exit 64
-                                                                                                                                                    gh pr merge --rebase --subject "Promotion Merge" "$URL"
-                                                                                                                                                    gh auth logout
-                                                                                                                                                    EOF
+                                                                                                                                                gh auth login --host-name github.com --with-token < "$TOKEN/secret"
+                                                                                                                                                URL="$( gh pr create --base main --head "$SQUASH_BRANCH" --title "Promotion" --body "Automated Promotion Merge" )" || exit 64
+                                                                                                                                                gh pr merge --rebase --subject "Promotion Merge" "$URL"
+                                                                                                                                                gh auth logout
+                                                                                                                                                EOF
                                                                                                                                                     gh auth login --host-name github.com --with-token < "$TOKEN/secret"
                                                                                                                                                     URL="$( gh pr create --base main --head "$SQUASH_BRANCH" --title "Promotion" --body "Automated Promotion Merge" )" || exit 64
                                                                                                                                                     gh pr merge --rebase --subject "Promotion Merge" "$URL"
