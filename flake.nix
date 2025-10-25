ce9e8b61525b627795ae2fd78c031176fbc466f1
Emory Merryman <emory.merryman@gmail.com>
2025-10-24 23:35:07 -0400

    fixed

--- flake.nix
+++ flake.nix
@@ -552,6 +552,15 @@
                                                         package = "nixpkgs#cowsay" ;
                                                     } ;
                                             in factory.check { expected-init = "/nix/store/hvma100pajisglzfgjh7gf0r1a6zxy0f-init/bin/init" ; mkDerivation = pkgs.stdenv.mkDerivation ; } ;
+                                    ephemeral-sad =
+                                        let
+                                            factory =
+                                                _ephemeral-bin
+                                                    {
+                                                        garbage-collection-root = "/build/garbage-collection-root" ;
+                                                        package = "nixpkgs#nosuchpackage" ;
+                                                    } ;
+                                            in factory.check { expected-init = "/nix/store/hvma100pajisglzfgjh7gf0r1a6zxy0f-init/bin/init" ; mkDerivation = pkgs.stdenv.mkDerivation ; } ;
                                     failure =
                                         _failure.check
                                             {
