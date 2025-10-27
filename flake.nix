be489bd5397c57c0727e63ea0c3cbd5aef925d86
Emory Merryman <emory.merryman@gmail.com>
2025-10-27 07:56:59 -0400

    personal

--- flake.nix
+++ flake.nix
@@ -95,7 +95,7 @@
                                                                 ignore :
                                                                     let
                                                                         x = _dot-ssh { } ;
-                                                                        in x.implementation { host-name = "mobile" ; port = 8022 ; strict-host-key-checking = true ; } ;
+                                                                        in x.implementation { host-name = "mobile" ; port = 8022 ; strict-host-key-checking = true ; identity-file = resources.foobar.secret ( setup : setup ) ; } ;
                                                             ephemeral =
                                                                 ignore :
                                                                     let
