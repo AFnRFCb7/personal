af76fe55f1c4bcfb1e64d94d88a690f1ae3d5a4d
Emory Merryman <emory.merryman@gmail.com>
2025-10-27 07:14:24 -0400

    personal

--- flake.nix
+++ flake.nix
@@ -123,9 +123,10 @@
                                                                                                         EPHEMERAL=${ resources.foobar.ephemeral ( setup : setup ) }
                                                                                                         ln --symbolic "$EPHEMERAL" /links
                                                                                                         ln --symbolic "$EPHEMERAL/ephemeral" /mount
-                                                                                                        SECRET=${ resources.foobar.secret ( setup : setup ) }
-                                                                                                        ln --symbolic "$SECRET" /links
-                                                                                                        ln --symbolic "$SECRET/secret" /mount
+                                                                                                        echo '${ resources.foobar.secret ( setup : setup ) } > /mount/secret
+                                                                                                        # SECRET=${ resources.foobar.secret ( setup : setup ) }
+                                                                                                        # ln --symbolic "$SECRET" /links
+                                                                                                        # ln --symbolic "$SECRET/secret" /mount
                                                                                                     '' ;
                                                                                             } ;
                                                                                     in "${ application }/bin/init" ;
