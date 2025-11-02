b4c984167d05319752f66256c902f16fccad31fb
Emory Merryman <emory.merryman@gmail.com>
2025-11-02 11:26:45 -0500

    personal

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
                                             redis = pkgs.redis ;
                                             resources = resources ;
                                             resources-directory = resources-directory ;
-                                            visitor = visitor.lib.implementation ;
+                                            visitor = _visitor.implementation ;
                                             writeShellApplication = pkgs.writeShellApplication ;
                                             yq-go = pkgs.yq-go ;
                                         } ;
