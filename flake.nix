df6fbee4661fd6f3d20ea3d3d167cfcad9fd4816
Emory Merryman <emory.merryman@gmail.com>
2025-11-04 08:00:11 -0500

    personal

--- flake.nix
+++ flake.nix
@@ -860,7 +860,7 @@
                                                     encrypted = ignore : "${ fixture }/age/encrypted/known-hosts.asc" ;
                                                     expected = "/nix/store/gciqcwms4g5z2imjafmpgd203adss1sw-init/bin/init" ;
                                                     identity = ignore : "${ fixture }/age/identity/private" ;
-                                                    failure = _failure.implementation ;
+                                                    failure = _failure.implementation "a720a5e7" ;
                                                     pkgs = pkgs ;
                                                } ;
                                                 visitor-happy =
