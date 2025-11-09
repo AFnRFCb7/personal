278960cec726599b309c2a39de5ded531f1e4fc3
Emory Merryman <emory.merryman@gmail.com>
2025-11-09 17:46:52 -0500

    personal

--- flake.nix
+++ flake.nix
@@ -209,7 +209,7 @@
                                                                                         {
                                                                                             configs =
                                                                                                 {
-                                                                                                    "core.sshCommand" = ssh ;
+                                                                                                    "core.sshCommand" = { pkgs , resources , self } : "${ pkgs.openssh }/bin/ssh -F ${ resources.production.dot-ssh ( setup : setup ) }" ;
                                                                                                     "user.email" = config.personal.repository.private.email ;
                                                                                                     "user.name" = config.personal.repository.private.name ;
                                                                                                 } ;
