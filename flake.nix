28adb8651fdf1f8c2add94b504c330813610627e
Emory Merryman <emory.merryman@gmail.com>
2025-11-09 18:02:58 -0500

    personal

--- flake.nix
+++ flake.nix
@@ -159,8 +159,8 @@
                                                                                     host-name = "192.168.1.192" ;
                                                                                     identity-file = { pkgs , resources , self } : { directory = resources.production.secrets.dot-ssh.mobile.identity-file ( setup : setup ) ; file = "secret" ; } ;
                                                                                     port = 8022 ;
-                                                                                    # strict-host-key-checking = true ;
-                                                                                    # user-known-hosts-file = { pkgs , resources , self } : { directory = resources.production.secrets.dot-ssh.mobile.user-known-hosts-file ( setup : setup ) ; file = "secret" ; } ;
+                                                                                    strict-host-key-checking = true ;
+                                                                                    user-known-hosts-file = { pkgs , resources , self } : { directory = resources.production.secrets.dot-ssh.mobile.user-known-hosts-file ( setup : setup ) ; file = "secret" ; } ;
                                                                                 } ;
                                                                         } ;
                                                             repository =
