69c15d67e890826cd9058535c95947e219de52a0
Emory Merryman <emory.merryman@gmail.com>
2025-11-09 09:36:10 -0500

    personal

--- flake.nix
+++ flake.nix
@@ -193,7 +193,7 @@
                                                                                                 ''
                                                                                                     export PKGS=${ pkgs.chromium }
                                                                                                     export SELF=${ self }
-
+                                                                                                    export RESOURCES=${ resources__.foobar.foobar ( self : self ) }
                                                                                                 '' ;
                                                                                         } ;
                                                                                 in "${ application }/bin/ssh" ;
