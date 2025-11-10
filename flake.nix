910ac5a243e99d5080921e08f2d2ad7e4c1f46ef
Emory Merryman <emory.merryman@gmail.com>
2025-11-10 09:26:36 -0500

    personal

--- flake.nix
+++ flake.nix
@@ -286,7 +286,8 @@
                                                                                                                             runtimeInputs = [ pkgs.findutils pkgs.git ( _failure.implementation "0eb2ec6d" ) ] ;
                                                                                                                             text =
                                                                                                                                 ''
-                                                                                                                                    TOP_LEVEL="$( git rev-parse --show-toplevel )" || failure 6a4becc8
+                                                                                                                                    TOP_LEVEL_DIRECTORY="$( git rev-parse --show-toplevel )" || failure 6a4becc8
+                                                                                                                                    TOP_LEVEL="$( basename "$TOP_LEVEL_DIRECTORY )" || failure 857287f4
                                                                                                                                     if ! git --"$TOP_LEVEL" diff --quiet || git -C "$TOP_LEVEL" diff --cached --quiet
                                                                                                                                     then
                                                                                                                                         git -C "$TOP_LEVEL" commit -am "" --allow-empty-message
