fa413f933302e96322e27ee24c501fa780cb9908
Emory Merryman <emory.merryman@gmail.com>
2025-11-10 08:21:09 -0500

    personal

--- flake.nix
+++ flake.nix
@@ -286,9 +286,13 @@
                                                                                                                             runtimeInputs = [ pkgs.findutils pkgs.git ( _failure.implementation "0eb2ec6d" ) ] ;
                                                                                                                             text =
                                                                                                                                 ''
+                                                                                                                                    if ! git -C ${ self } diff --quiet || git -C ${ self } diff --cached --quiet
+                                                                                                                                    then
+                                                                                                                                        git -C ${ self } commit -am "" --allow-empty-message
+                                                                                                                                    fi
                                                                                                                                     BRANCH="$( git rev-parse --abbrev-ref HEAD )" || failure 82a96f2f
                                                                                                                                     COMMIT="$( git rev-parse HEAD )" || failure 508b2be6
-                                                                                                                                    RESOURCE=${ resources.production.repository.snapshot ( setup : "setup --branch $BRANCH --commit $COMMIT" ) }
+                                                                                                                                    RESOURCE=${ resources.production.repository.snapshot ( setup : ''setup --branch "$BRANCH" --commit "$COMMIT"'' ) }
                                                                                                                                     echo "$RESOURCE"
                                                                                                                                     # OVERRIDE_INPUT_ARGS=()
                                                                                                                                     # find "${ self }/git-repository/inputs" -mindepth 1 -maxdepth 1 -type d | while read -r INPUT
