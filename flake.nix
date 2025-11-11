bf2f27be2cac752abfbf617bfa7e7dd3b0d1cb64
Emory Merryman <emory.merryman@gmail.com>
2025-11-11 09:31:06 -0500

    personal

--- flake.nix
+++ flake.nix
@@ -443,8 +443,8 @@
                                                                                                                                     git -C "$TOP_LEVEL" push origin HEAD 2>&1
                                                                                                                                     BRANCH="$( git -C "$TOP_LEVEL" rev-parse --abbrev-ref HEAD )" || failure 82a96f2f
                                                                                                                                     COMMIT="$( git -C "$TOP_LEVEL" rev-parse HEAD )" || failure 508b2be6
-                                                                                                                                    RESOURCE=${ resources.production.repository.snapshot ( setup : ''${ setup } --branch "$BRANCH" --commit "$COMMIT" "${ builtins.concatStringsSep "" [ "$" "{" "INPUTS[@]" "}" ] }"'' ) }
-                                                                                                                                    echo "$RESOURCE/git-repository"
+                                                                                                                                    # RESOURCE=${ resources.production.repository.snapshot ( setup : ''${ setup } --branch "$BRANCH" --commit "$COMMIT" "${ builtins.concatStringsSep "" [ "$" "{" "INPUTS[@]" "}" ] }"'' ) }
+                                                                                                                                    # echo "$RESOURCE/git-repository"
                                                                                                                                 '' ;
                                                                                                                         } ;
                                                                                                                 in "!${ application }/bin/snapshot" ;
