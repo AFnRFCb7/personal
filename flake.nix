89862ce89171156a431fc45c4d85f3ce9eaf1680
Emory Merryman <emory.merryman@gmail.com>
2025-11-13 07:53:44 -0500

    personal

--- flake.nix
+++ flake.nix
@@ -1093,7 +1093,7 @@
                                                                                     runtimeInputs = [ pkgs.coreutils pkgs.jetbrains.idea-community ] ;
                                                                                     text =
                                                                                         ''
-                                                                                            STUDIO=${ resources__.production.repository.studio ( setup : "${ setup } 91f61dbf" ) }
+                                                                                            STUDIO=${ resources__.production.repository.studio ( setup : "${ setup } 6acc2071" ) }
                                                                                             idea-community "$STUDIO/git-repository"
                                                                                         '' ;
                                                                                 }
