e45b12f40666e385b4a1b392005d92af297181e5
Emory Merryman <emory.merryman@gmail.com>
2025-11-17 14:49:34 -0500

    debug

--- flake.nix
+++ flake.nix
@@ -394,8 +394,8 @@
                                                                                                                     in
                                                                                                                         ''
                                                                                                                             makeWrapper ${ build-vm }/bin/build-vm /mount/stage/build-vm --set MOUNT "${ mount }"
-                                                                                                                            # makeWrapper ${ build-vm-with-bootloader }/bin/build-vm-with-bootloader /mount/stage/build-vm-with-bootloader --set MOUNT "${ mount }"
-                                                                                                                            # makeWrapper ${ check }/bin/check /mount/stage/check --set MOUNT "${ mount }"
+                                                                                                                            makeWrapper ${ build-vm-with-bootloader }/bin/build-vm-with-bootloader /mount/stage/build-vm-with-bootloader --set MOUNT "${ mount }"
+                                                                                                                            makeWrapper ${ check }/bin/check /mount/stage/check --set MOUNT "${ mount }"
                                                                                                                             # makeWrapper ${ scratch }/bin/scratch /mount/stage/scratch --set MOUNT "${ mount }"
                                                                                                                             # makeWrapper ${ switch }/bin/switch /mount/stage/switch --set MOUNT "${ mount }"
                                                                                                                             # makeWrapper ${ test }/bin/test /mount/stage/test --set MOUNT "${ mount }"
