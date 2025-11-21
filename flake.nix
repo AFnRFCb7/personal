a7688a320cefb2674b18dafb5d34ed45116d0f2d
Emory Merryman <emory.merryman@gmail.com>
2025-11-20 20:32:31 -0500

    wtf

--- flake.nix
+++ flake.nix
@@ -347,11 +347,6 @@
 														                                                                        DOT_SSH=${ resources.production.dot-ssh ( self : self ) }
 														                                                                        root-resource "$DOT_SSH"
 														                                                                        ln --symbolic "$DOT_SSH/dot-ssh" "${ mount }/stage/dot-ssh"
-														                                                                        find "${ mount }/repository/inputs" -mindepth 1 -maxdepth 1 -type d | while read -r INPUT
-														                                                                        do
-														                                                                            INPUT_NAME="$( basename "$INPUT" )" || failure d9d05c9e
-														                                                                            nix flake update --flake "${ mount }/repository/inputs" --update-input "$INPUT_NAME"
-														                                                                        done
 														                                                                        # make-wrapper ${ flake-check } /mount/stage/check "${ mount }"
 													                                                                        '' ;
                                                                                                         } ;
