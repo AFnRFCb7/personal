976e8c0923de26ca3aa65c59898736739d5ee39e
Emory Merryman <emory.merryman@gmail.com>
2025-11-27 10:06:01 -0500

    exit

--- flake.nix
+++ flake.nix
@@ -1229,7 +1229,7 @@
                                                                 strict-host-key-checking = false ;
                                                                 port = 8022 ;
                                                                 identity-file = ./. ;
-                                                                host = "eedaca3e" ;
+                                                                host-name = "eedaca3e" ;
                                                             } ;
                                                     } ;
                                                 expected = "/nix/store/al5rjp5h9n8g38hbx53rkwj0hhczwcvz-init/bin/init" ;
