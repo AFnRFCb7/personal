72d0dde2d3c4b00c17e7dbf0c08ecf07cd49707f
Emory Merryman <emory.merryman@gmail.com>
2025-11-27 10:59:37 -0500

    wtf

--- flake.nix
+++ flake.nix
@@ -1234,7 +1234,7 @@
                                                                 host-name = "eedaca3e" ;
                                                             } ;
                                                     } ;
-                                                expected = "/nix/store/j241aqqsdxv6b99wdc9hv9wwbwjky0kq-init/bin/init" ;
+                                                expected = "/nix/store/886vjyl68w6sfm0pqmz4cr5w0mh6x8fx-init/bin/init" ;
                                                 mount = "45e10de89f958caa832bb6b4dd740aae2b5d0e4e688286dab3bb5881ca44eab29e663a0c068390a39a85915e4222ccbf19e85e2602fb431e608ef6cbc6d95a51" ;
                                                 pkgs = pkgs ;
                                                 implementation-resources =
