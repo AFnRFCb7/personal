4fefc8bf6b86e4c43e0b4963edb01fef160f9ea3
Emory Merryman <emory.merryman@gmail.com>
2025-11-29 10:45:08 -0500

    wtf

--- flake.nix
+++ flake.nix
@@ -143,6 +143,7 @@
                                                                                                     '' ;
                                                                                             } ;
                                                                                     in "${ application }/bin/init" ;
+                                                                        resolutions = [ "alpha" "beta" "gamma" ] ;
                                                                         targets = [ "dot-gnupg" "dot-ssh" "git-repository" "secret" ] ;
                                                                         transient = true ;
                                                                     } ;
@@ -1149,7 +1150,7 @@
                                                                                                                                     chmod 0500 "/home/${ config.personal.name }/resources/quarantine/$INDEX/log"
                                                                                                                                     mkdir --parents "/home/${ config.personal.name }/resources/quarantine/$INDEX/resolve"
                                                                                                                                     RESOLUTIONS="$( yq eval ".description.seed.resolutions" <<< "$PAYLOAD" )" || failure 632e8f4e
-                                                                                                                                    for RESOLUTION in "RESOLUTIONS"
+                                                                                                                                    for RESOLUTION in "$RESOLUTIONS"
                                                                                                                                     do
                                                                                                                                         export MODE=manual
                                                                                                                                         export RESOLUTION
