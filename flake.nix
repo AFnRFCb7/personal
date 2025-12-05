ae894a9d06c83c84d7a37835c40f302d702d3a22
Emory Merryman <emory.merryman@gmail.com>
2025-12-05 13:23:12 -0500

    ack

--- flake.nix
+++ flake.nix
@@ -1431,7 +1431,7 @@
                                                                                                                                         else
                                                                                                                                             mkdir --parents "/home/${ config.personal.name }/resources/quarantine/$INDEX/release"
                                                                                                                                             RELEASE_RESOLUTIONS_JSON_1="$( printf '"%s",'  "${ builtins.concatStringsSep "" [ "$" "{" "RESOLUTIONS[*]}" "}" ] }" )" || failure 456dd0ed
-                                                                                                                                            RELEASE_RESOLUTIONS_JSON="[${ builtins.concatStringsSep "" [ "$" "{" "RELEASE_RESOLUTIONS_JSON_1," "}" ] }]"
+                                                                                                                                            RELEASE_RESOLUTIONS_JSON="[${ builtins.concatStringsSep "" [ "$" "{" "RELEASE_RESOLUTIONS_JSON_1" "}" ] }]"
                                                                                                                                             STANDARD_ERROR="$( cat "$STANDARD_ERROR_FILE" )" || failure be48c573
                                                                                                                                             STANDARD_OUTPUT="$( cat "$STANDARD_OUTPUT_FILE" )" || failure 83137e6b
                                                                                                                                             echo "45357e98 $0 RESOLUTIONS=$RELEASE_RESOLUTIONS_JSON"
