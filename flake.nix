9f1b9ef77241ebb2ed3be93da4f66759a191b151
Emory Merryman <emory.merryman@gmail.com>
2025-12-06 05:56:21 -0500

    awk

--- flake.nix
+++ flake.nix
@@ -2099,7 +2099,7 @@
                                             _resource-logger.check
                                                 {
                                                     channel = "48950186" ;
-                                                    expected = "/nix/store/ic1ambwjxnycd4n1q3j64z49d02r3s2v-implementation/bin/implementation" ;
+                                                    expected = "40546df7" ;
                                                     log-directory = "87d087a1" ;
                                                     log-file = "c9f433d1" ;
                                                     log-lock = "00e970a8" ;
