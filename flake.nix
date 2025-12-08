7a52f17b942e104f1abd498f78ed34cd3cb5d78e
Emory Merryman <emory.merryman@gmail.com>
2025-12-07 19:49:18 -0500

    aws

--- flake.nix
+++ flake.nix
@@ -1784,7 +1784,7 @@
                                                        transient = false ;
                                                  } ;
                                          resource-logger = _resource-logger.check { expected = "/nix/store/6iyrf556ps24jvigrx7jgfvyi4jvrlmk-resource-logger/bin/resource-logger" ; } ;
-                                         resource-releaser = _resource-releaser.check { } ;
+                                         resource-releaser = _resource-releaser.check { expected = "/nix/store/gzzp0p0bhci6cr5b58ni3jgrql8dwbga-resource-releaser/bin/resource-releaser" ; } ;
                                          resource-resolver = _resource-resolver.check { expected = "/nix/store/qfmq26b2x9x66n3fc4bfqxvm0r1amiag-resource-resolver/bin/resource-resolver" ; } ;
                                         secret =
                                             _secret.check
