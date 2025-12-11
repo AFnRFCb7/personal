afea6503b2b363442f267f2f43046a05ae997494
Emory Merryman <emory.merryman@gmail.com>
2025-12-11 16:30:08 -0500

    asap

--- flake.nix
+++ flake.nix
@@ -1874,8 +1874,6 @@
                                                  {
                                                      arguments = [ "ceb405a144a10b8efca63d9d950ce2b92bb2997ab44a9588ca740b3540a9a532a6b959a0d990dd469a63b16eb7600991bb7a1ef2b79d697b43e17134cbccec6c" "cdca67397f32d23a379284468e099b96c5b53d62659faf4d48dfc650bea444d6bc450b7eefee9b273c12672b9008fa6a077b15efb676b35f9912de977f54724d" ] ;
                                                      diffutils = pkgs.diffutils ;
-                                                     expected-resource-dependencies = [ "5552fc1d63b863ab116115819c2f0f2f2fb7e47fc59fd4ef3e99651b982f54b050afa38207f9d74d18a7f6e167debc1c9aad4962b22340091c45878cc1abd75c" ] ;
-                                                     expected-store-dependencies = [ ] ;
                                                      expected-index = "0000000437766789" ;
                                                      expected-originator-pid = 45 ;
                                                      expected-provenance = "new" ;
