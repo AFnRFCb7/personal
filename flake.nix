2752c715e88b9559a08d553a51ffe91f0aa57f86
Emory Merryman <emory.merryman@gmail.com>
2025-12-12 14:21:42 -0500

    abc

--- flake.nix
+++ flake.nix
@@ -495,9 +495,8 @@
                                                                                                                             root ${ pkgs.openssh }
                                                                                                                             DOT_SSH=${ resources.production.dot-ssh ( setup : setup ) }
                                                                                                                             root "$DOT_SSH"
-                                                                                                                            mkdir --parents ${ mount }/.ssh
+                                                                                                                            mkdir --parents "${ mount }/.ssh"
                                                                                                                             ln --symbolic "$DOT_SSH/config" ${ mount }/.ssh/config
-                                                                                                                            mkdir --parents ${ mount }/bin
                                                                                                                             wrap ssh bin/ssh --set MOUNT "${ mount }"
                                                                                                                         '' ;
                                                                                                         } ;
