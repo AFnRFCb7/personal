09b86793a56ed9ac40ba0acb7a74692438f38387
Emory Merryman <emory.merryman@gmail.com>
2025-12-12 20:11:59 -0500

    abc

--- flake.nix
+++ flake.nix
@@ -1135,8 +1135,8 @@
                                                                                                                                 git config user.email "${ config.personal.repository.private.email }"
                                                                                                                                 git config user.name "${ config.personal.repository.private.name }"
                                                                                                                             done
-                                                                                                                            wrap ${ mutable-check } stage/bin/mutable-check 0500 --literal "MUTABLE_SNAPSHOT" --set MOUNT "${ mount }"
-                                                                                                                            wrap ${ mutable-snapshot } stage/bin/mutable-snapshot 0500 --literal "MUTABLE_SNAPSHOT" --set MOUNT "${ mount }"
+                                                                                                                            wrap ${ mutable-check } stage/bin/mutable-check 0500 --literal MUTABLE_SNAPSHOT --set MOUNT "${ mount }"
+                                                                                                                            wrap ${ mutable-snapshot } stage/bin/mutable-snapshot 0500 --literal BRANCH --literal COMMIT --literal "MUTABLE_SNAPSHOT" --set MOUNT "${ mount }"
 
                                                                                                                             wrap ${ mutable-nurse } stage/mutable-nurse 0500 --literal INPUT --literal COMMIT --literal REPO_NAME --literal USER_NAME --set MOUNT "${ mount }"
                                                                                                                             wrap ${ mutable-rebase } stage/mutable-rebase 0500 --literal FAILURE --literal STATUS --set MOUNT "${ mount }"
