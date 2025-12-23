a44bccf9f57f2065a51ee6c3455889d3841b7a5e
Emory Merryman <emory.merryman@gmail.com>
2025-12-23 09:21:18 -0500

    abc

--- flake.nix
+++ flake.nix
@@ -580,11 +580,11 @@
                                                                                                                                     wrap ${ mutable-mirror } stage/bin/mutable-mirror 0500 --literal BRANCH
                                                                                                                                     wrap ${ mutable-snapshot } stage/bin/mutable-snapshot 0500 --inherit INDEX --set-plain MOUNT "${ mount }"
                                                                                                                                     wrap ${ mutable-build-vm "build-test" } stage/bin/mutable-test 0500 --inherit INDEX --set-plain MOUNT "${ mount }"
-                                                                                                                                    mkdir --parents /mount/stage/build-vm/share
-                                                                                                                                    mkdir --parents /mount/stage/build-vm-with-bootloader/share
-                                                                                                                                    mkdir --parents /mount/stage/test
-                                                                                                                                    mkdir --parents /mount/stage/switch
-                                                                                                                                    mkdir --parents /mount/stage/converge
+                                                                                                                                    # mkdir --parents /mount/stage/build-vm/share
+                                                                                                                                    # mkdir --parents /mount/stage/build-vm-with-bootloader/share
+                                                                                                                                    # mkdir --parents /mount/stage/test
+                                                                                                                                    # mkdir --parents /mount/stage/switch
+                                                                                                                                    # mkdir --parents /mount/stage/converge
                                                                                                                                 '' ;
                                                                                                                 } ;
                                                                                                             in "${ application }/bin/post-setup" ;
