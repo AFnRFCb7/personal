2d4a0d1ae82498b4f919c41a615381ec7406f38e
Emory Merryman <emory.merryman@gmail.com>
2025-12-25 21:19:14 -0500

    wtf

--- flake.nix
+++ flake.nix
@@ -665,7 +665,8 @@
                                                                                                                             wrap ${ ssh-command } stage/ssh/command 0500 --inherit INDEX --set-plain MOUNT "${ mount }"
                                                                                                                             wrap ${ mutable-snapshot.submodule } stage/alias/submodules/mutable-switch 0500 --inherit INDEX --set-plain MOUNT "${ mount }"
                                                                                                                             echo 88097632 "$0" >&2
-                                                                                                                            DOT_SSH=${ resources.production.dot-ssh ( setup : setup ) }
+                                                                                                                            # DOT_SSH=${ resources.production.dot-ssh ( setup : setup ) }
+                                                                                                                            DOT_SSH="$( mktemp -d )" || failure d345afcf
                                                                                                                             echo eb135223 >&2
                                                                                                                             root "$DOT_SSH"
                                                                                                                             wrap "$DOT_SSH/config" stage/ssh/config 0400
