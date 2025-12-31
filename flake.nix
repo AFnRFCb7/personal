9e27991f643984146b6cf6acc5eb5d3f9d812c40
Emory Merryman <emory.merryman@gmail.com>
2025-12-30 19:44:12 -0500

    abc

--- flake.nix
+++ flake.nix
@@ -1086,7 +1086,8 @@
                                                                                                                                     mkdir --parents /mount/stage/artifacts/check
                                                                                                                                     mkdir --parents /mount/stage/artifacts/test
                                                                                                                                     mkdir --parents /mount/stage/artifacts/switch
-                                                                                                                                    git submodule synch update --init --recursive
+                                                                                                                                    git submodule synch >&2
+                                                                                                                                    git submodule update --init --recursive >&2
                                                                                                                                     git submodule foreach "submodule" >&2
                                                                                                                                     UUID="$( uuidgen | sha512sum )" || failure 839b0e7b
                                                                                                                                     NEW_BRANCH="$( echo "scratch/$UUID" | cut --characters 1-64 )" || failure 1bc074b2
