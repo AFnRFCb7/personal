20f60516cf17a5bf0a22773ffd18cbbc386a0d8f
Emory Merryman <emory.merryman@gmail.com>
2026-01-07 05:25:54 -0500

    abc

--- flake.nix
+++ flake.nix
@@ -1728,7 +1728,7 @@
                                                                 pads =
                                                                     lib.mkOption
                                                                         {
-                                                                            type = lib.types.attrsOf ( lib.types.funcTo lib.types.str ) ;
+                                                                            type = lib.types.attrsOf ( lib.types.functionTo lib.types.str ) ;
                                                                             default =
                                                                                 {
                                                                                     emory =
