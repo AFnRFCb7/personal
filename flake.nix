95957dad26cbfaba11965a60a84c00d8525212a8
Emory Merryman <emory.merryman@gmail.com>
2026-01-08 10:23:59 -0500

    abc

--- flake.nix
+++ flake.nix
@@ -1859,7 +1859,7 @@
                                                                                                                                             ] ;
                                                                                                                                         text =
                                                                                                                                             ''
-                                                                                                                                                CONFIG_RESOURCE=${ resources__.production.repository.pad.chromium.config }
+                                                                                                                                                CONFIG_RESOURCE=${ resources__.production.repository.pads.chromium.config }
                                                                                                                                                 DATA_RESOURCE="$( mktemp -d )" || failure b40fd012
                                                                                                                                                 export DATA_RESOURCE
                                                                                                                                                 mkdir "$DATA_RESOURCE/repository"
