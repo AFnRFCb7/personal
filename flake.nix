e5fbda184e7be5af696b5608b8f1791c139b2461
Emory Merryman <emory.merryman@gmail.com>
2026-01-13 08:14:19 -0500

    abc

--- flake.nix
+++ flake.nix
@@ -1841,7 +1841,7 @@
                                                                                                                                                             runtimeInputs = [ pkgs.gnupg ] ;
                                                                                                                                                             text =
                                                                                                                                                                 ''
-                                                                                                                                                                    DOT_GNUPG=${ resources__.production.dot-gnupg }
+                                                                                                                                                                    DOT_GNUPG=${ resources__.production.dot-gnupg { } }
                                                                                                                                                                     export GNUPGHOME="$DOT_GNUPG/dot-gnupg"
                                                                                                                                                                     if [[ -t 0 ]]
                                                                                                                                                                     then
