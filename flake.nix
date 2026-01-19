f5a63810f6447493f83b87f4e00b49720057d7bc
Emory Merryman <emory.merryman@gmail.com>
2026-01-18 21:49:04 -0500

    abc

--- flake.nix
+++ flake.nix
@@ -1994,6 +1994,7 @@
                                                                 packages =
                                                                     [
                                                                         pkgs.gh
+                                                                        pkgs.yq
                                                                         ( _failure.implementation "762e3818" )
                                                                         (
                                                                             pkgs.writeShellApplication
