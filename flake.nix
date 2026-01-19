2cdfa334e798d6592c5b1d9b241c0160e11ff786
Emory Merryman <emory.merryman@gmail.com>
2026-01-19 17:50:45 -0500

    abc

--- flake.nix
+++ flake.nix
@@ -2590,7 +2590,7 @@
                                                             } ;
                                                     } ;
                                                 expected = "/nix/store/05f5bx3jmjp8l85paq330klvrh912236-init/bin/init" ;
-                                                mount "271a376c" ;
+                                                mount = "271a376c" ;
                                                 pkgs = pkgs ;
                                                 implementation-resources =
                                                     {
