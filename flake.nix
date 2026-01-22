cd1515c1085400d48da74eb7eb925527f99d477d
Emory Merryman <emory.merryman@gmail.com>
2026-01-21 20:41:56 -0500

    abc

--- flake.nix
+++ flake.nix
@@ -1927,7 +1927,7 @@
                                                                                                                                     tiny =
                                                                                                                                         ''
                                                                                                                                             PATH="${ pkgs.bash }/bin:${ pkgs.gawk }/bin:${ pkgs.coreutils }/bin:${ bin.ssh }"
-                                                                                                                                            source ${ pkgs.writeTextFile "autocomplete" autocomplete.ssh }
+                                                                                                                                            source ${ pkgs.writeTextFile { name = "autocomplete" ; text = autocomplete.ssh ; } }
                                                                                                                                             export NAME="${ config.personal.description }"
                                                                                                                                         '' ;
                                                                                                                                 } ;
@@ -1935,7 +1935,7 @@
                                                                                                                         name : value :
                                                                                                                             ''
                                                                                                                                 mkdir --parents /home/${ config.personal.name }/pads/${ name }
-                                                                                                                                ln --symbolic ${ pkgs.writeTextFile "autocomplete" value } /home/${ config.personal.name }/pads/${ name }/.envrc
+                                                                                                                                ln --symbolic ${ pkgs.writeTextFile { name = "autocomplete" ; text = value ; } } /home/${ config.personal.name }/pads/${ name }/.envrc
                                                                                                                             '' ;
                                                                                                                     in
                                                                                                                         builtins.concatStringsSep "" ( builtins.attrValues ( builtins.mapAttrs mapper envrc ) ) ;
