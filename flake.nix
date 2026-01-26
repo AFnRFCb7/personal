6ac1671888970060f740ec6df9f31605f17ffc78
Emory Merryman <emory.merryman@gmail.com>
2026-01-26 17:54:03 -0500

    abc

--- flake.nix
+++ flake.nix
@@ -576,8 +576,9 @@
                                                                                                         ''
                                                                                                             git config core.sshCommand "$MOUNT/stage/ssh/command"
                                                                                                             wrap ${ application }/bin/ssh stage/ssh/command 0500 --literal-plain "@" --inherit-plain MOUNT --literal-plain PATH
-                                                                                                            echo
+                                                                                                            echo c347092e
                                                                                                             DOT_SSH=${ resources.production.dot-ssh { } }
+                                                                                                            echo 7905c77d
                                                                                                             root "$DOT_SSH"
                                                                                                             wrap "$DOT_SSH/config" stage/ssh/config 0400
                                                                                                         '' ;
@@ -784,7 +785,7 @@
                                                                                                                     text =
                                                                                                                         ''
                                                                                                                             echo 66972a21
-                                                                                                                            ${ ssh pkgs resources sequential wrap }
+
                                                                                                                             echo ba450dc8
                                                                                                                             git remote add origin ${ config.personal.secrets.remote }
                                                                                                                             echo 786dcd65
