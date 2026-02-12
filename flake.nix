6ddb71f5ca09ea504086c8674d5918da4d3ba8b6
Emory Merryman <emory.merryman@gmail.com>
2026-02-12 14:29:49 -0500

    abc

--- flake.nix
+++ flake.nix
@@ -2086,7 +2086,7 @@
                                                                                                                                                                 prompt "mutable-test 1"
                                                                                                                                                                 SEQUENCE="$( sequential )" || failure ae7e6cd4y
                                                                                                                                                                 REPO_2="$( studio "$SEQUENCE" ) || failure c26c59b5
-                                                                                                                                                                PARENT_2="$( dirname "$REPO_2" ) || failure aa07751f
+                                                                                                                                                                PARENT_2="$( dirname "$REPO_2" )" || failure aa07751f
                                                                                                                                                                 # PARENT_2="$( "$SETUP" "$SEQUENCE" )" || failure 1ba93b40
                                                                                                                                                                 STUDIO_2="$PARENT_2/repository"
                                                                                                                                                                 BRANCH="$( git -C "$STUDIO_1" rev-parse --abbrev-ref HEAD )" || failure 89dfeef9
