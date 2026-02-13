174120825c98b7e9a7f3d66c8aca70dae00226d2
Emory Merryman <emory.merryman@gmail.com>
2026-02-13 10:36:33 -0500

    abc

--- flake.nix
+++ flake.nix
@@ -3411,7 +3411,11 @@
                                                                         timeout = lib.mkOption { default = 60 * 60 ; type = lib.types.int ; } ;
                                                                         timeout2 = lib.mkOption { default = 60 ; type = lib.types.int ; } ;
                                                                     } ;
-                                                                mobile = lib.mkOption { default = "192.168.1.192" ; type = lib.types.str ; } ;
+                                                                mobile =
+                                                                    {
+                                                                        ip = lib.mkOption { default = "192.168.1.192" ; type = lib.types.str ; } ;
+                                                                        port = lib.mkOption { default = 8022 ; type = lib.types.int ; } ;
+                                                                    } ;
                                                                 name = lib.mkOption { type = lib.types.str ; } ;
                                                                 pass =
                                                                     {
