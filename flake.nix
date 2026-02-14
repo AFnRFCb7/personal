a2b549a8f19b219216f656cf5391da42fe7ec88b
Emory Merryman <emory.merryman@gmail.com>
2026-02-14 09:05:06 -0500

    debug

--- flake.nix
+++ flake.nix
@@ -1895,7 +1895,7 @@
                                                                                                                                         mkdir --parents /mount/repository
                                                                                                                                         cd /mount/repository
                                                                                                                                         git init
-                                                                                                                                        ${ builtins.concatStringsSep "\n" ( builtins.attrValues ( builtins.mapAttrs ( name : value : ''git config alias.${ name } "${ builtins.toString value }"'' ) root ) ) }
+                                                                                                                                        ${ builtins.concatStringsSep "\n" ( builtins.attrValues ( builtins.mapAttrs ( name : value : ''git config alias.${ name } "${ builtins.toString value }"'' ) root.xxx ) ) }
                                                                                                                                     '' ;
                                                                                                                     } ;
                                                                                                             in "${ application }/bin/init" ;
