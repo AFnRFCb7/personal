8f48c6bc258474962f79b58bbd87b71e8d12373e
Emory Merryman <emory.merryman@gmail.com>
2026-02-16 19:05:20 -0500

    WTF

--- flake.nix
+++ flake.nix
@@ -1954,7 +1954,7 @@
                                                                                                                                     '' ;
                                                                                                                     } ;
                                                                                                             in "${ application }/bin/init" ;
-                                                                                                targets = [ "repository" ] ;
+                                                                                                targets = [ "repository" "stage" ] ;
                                                                                             } ;
                                                                                 } ;
                                                                         } ;
