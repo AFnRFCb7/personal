fbf453e1f5cf246a49296df67950d2d53072c58c
Emory Merryman <emory.merryman@gmail.com>
2026-02-19 11:29:34 -0500

    abc

--- flake.nix
+++ flake.nix
@@ -390,7 +390,7 @@
                                                                                                                             pkgs.writeShellApplication
                                                                                                                                 {
                                                                                                                                     name = name ;
-                                                                                                                                    runtimeInputs = builtins.concatLists [ ( runtimeInputs pkgs ) [ root ] ] ;
+                                                                                                                                    runtimeInputs = builtins.concatLists [ ( runtimeInputs pkgs ) [ failure ] ] ;
                                                                                                                                     text =
                                                                                                                                         let
                                                                                                                                             list =
@@ -516,24 +516,27 @@
                                                                                         script =
                                                                                             let
                                                                                                 secret =
-                                                                                                    pkgs.writeShellApplication
-                                                                                                        {
-                                                                                                            name = "secret" ;
-                                                                                                            runtimeInputs = [ pkgs.coreutils failure ] ;
-                                                                                                            text =
-                                                                                                                ''
-                                                                                                                    NAME="$1"
-                                                                                                                    ALLOWED=( "dot-gnupg/ownertrust" "dot-gnupg/secret-keys" "dot-ssh/github/known-hosts" "dot-ssh/github/identity" "dot-ssh/mobile/known-hosts" "dot-ssh/mobile/identity" "github/token" )
-                                                                                                                    if [[ ! " ${ builtins.concatStringsSep "" [ "$" "{" "ALLOWED[*]" "}" ] }" =~ " $NAME " ]]
-                                                                                                                    then
-                                                                                                                        failure da86aba0 "NAME=$NAME"
-                                                                                                                    fi
-                                                                                                                    cat > "$SECRETS/$NAME.asc.age"
-                                                                                                                    export GIT_SSH_COMMAND="${ pkgs.openssh }/bin/ssh -F $DOT_SSH/config"
-                                                                                                                    git commit --verbose
-                                                                                                                '' ;
-                                                                                                        } ;
-                                                                                                in ''echo "$SECRETS/plain"'' ;
+                                                                                                    let
+                                                                                                        application =
+                                                                                                            pkgs.writeShellApplication
+                                                                                                                {
+                                                                                                                    name = "secret" ;
+                                                                                                                    runtimeInputs = [ pkgs.coreutils failure ] ;
+                                                                                                                    text =
+                                                                                                                        ''
+                                                                                                                            NAME="$1"
+                                                                                                                            ALLOWED=( "dot-gnupg/ownertrust" "dot-gnupg/secret-keys" "dot-ssh/github/known-hosts" "dot-ssh/github/identity" "dot-ssh/mobile/known-hosts" "dot-ssh/mobile/identity" "github/token" )
+                                                                                                                            if [[ ! " ${ builtins.concatStringsSep "" [ "$" "{" "ALLOWED[*]" "}" ] }" =~ " $NAME " ]]
+                                                                                                                            then
+                                                                                                                                failure da86aba0 "NAME=$NAME"
+                                                                                                                            fi
+                                                                                                                            cat > "$SECRETS/$NAME.asc.age"
+                                                                                                                            export GIT_SSH_COMMAND="${ pkgs.openssh }/bin/ssh -F $DOT_SSH/config"
+                                                                                                                            git commit --verbose
+                                                                                                                        '' ;
+                                                                                                                } ;
+                                                                                                        in "${ application }/bin/secret" ;
+                                                                                                in ''${ secret } "$1"'' ;
                                                                                         variables =
                                                                                             {
                                                                                                 DOT_SSH = resources : resources.production.dot-ssh { failure = 24402 ; } ;
