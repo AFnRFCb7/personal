6db1a4c1df9347bfd819ea54f7f9d1d643c9bbd2
Emory Merryman <emory.merryman@gmail.com>
2026-02-21 08:12:55 -0500

    abc

--- flake.nix
+++ flake.nix
@@ -3274,6 +3274,7 @@
                                                         arguments = [ "ceb405a144a10b8efca63d9d950ce2b92bb2997ab44a9588ca740b3540a9a532a6b959a0d990dd469a63b16eb7600991bb7a1ef2b79d697b43e17134cbccec6c" "cdca67397f32d23a379284468e099b96c5b53d62659faf4d48dfc650bea444d6bc450b7eefee9b273c12672b9008fa6a077b15efb676b35f9912de977f54724d" ] ;
                                                         diffutils = pkgs.diffutils ;
                                                         expected = { } ;
+                                                        expected-resource = "/build/resources/mounts/0000000311691948" ;
                                                         init =
                                                             { failure , pid , pkgs , resources , root , seed , sequential , wrap } :
                                                                 let
